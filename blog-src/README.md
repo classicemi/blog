052ec9cbd16b29b1e831746abd9432b46e720af3
classicemi <wushuang_1227@163.com>
2016-12-12 14:12:01 +0800

    blog src

---
+++ blog-src/README.md
@@ -0,0 +1,17 @@
+# docs
+> A vue project.
+
+## Development
+
+```shell
+npm i cooking-cli -g
+npm run dev
+```
+
+## Production
+```
+npm run dist
+```
+
+## License
+ISC
---
+++ blog-src/src/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>docs</h1>
+    <p>A vue project.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  };
+</script>
